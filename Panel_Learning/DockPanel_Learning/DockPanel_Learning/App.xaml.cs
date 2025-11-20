96ddc1c6aaad47e6320db30b2fab536eb0ded44c
김상훈 <yofp1937@naver.com>
2025-11-20 19:45:37 +0900

    Panel 기초

---
+++ Panel_Learning/DockPanel_Learning/DockPanel_Learning/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DockPanel_Learning
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Panel_Learning/WrapPanel_Learning/WrapPanel_Learning/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WrapPanel_Learning
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
